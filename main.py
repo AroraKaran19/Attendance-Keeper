c06629223c69ec2095e8e848d18fb0729bc3ad0f
AroraKaran19 <karora0319@gmail.com>
2022-02-03 01:33:33 +0530

    change

--- main.py
+++ main.py
@@ -1,26 +1,10 @@
 #Attendance Keeper
 #Karan Arora
 
-from PyQt5 import QtWidgets, QtGui
-from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5.QtGui import *
+import tkinter as tk
 import sys
+import os
 
-def main_window():
-    app = QApplication(sys.argv)
-    win = QMainWindow()
-    win.setGeometry(200, 200, 500, 500)
-    win.setWindowTitle('Attendance Keeper')
-    win.setFixedSize(500, 500)
-    win.setStyleSheet("background-color: yellow;")
+os_name = os.name
 
-    heading = QtWidgets.QLabel(win)
-    heading.setText("Attendance Keeper")
-    heading.setFont(QFont('helvetica', 20))
-    heading.adjustSize()
-    heading.move(140, 20)
 
-    win.show()
-    sys.exit(app.exec())
-
-main_window()
